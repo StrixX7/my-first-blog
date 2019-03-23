2863cef9f83a37299e97b5e79aeb5f12c15318f6
camilo <camilo.online.com>
2019-03-23 13:58:09 -0500

    Mi aplicación Django Girls, primer commit

---
+++ python_intro.py
@@ -0,0 +1,8 @@
+def hi(nombre):
+	print("Hola " + nombre + "!!!!!!!")
+
+boys = ['camilo', 'david', 'alberto', 'santiago']
+for nombre in boys:	
+	hi(nombre)
+	print("Siguiente chico")
+	
---
+++ revertir.py
@@ -0,0 +1,4 @@
+def transformar_cadena(cadena):
+      cadena= cadena[::-1]
+      print (cadena)
+transformar_cadena("mario")
